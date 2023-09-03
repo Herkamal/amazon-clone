b342beed4e46846a219765a689f14ba7eeeda562
Herkamal <herkamalrandhawa@gmail.com>
2023-09-03 12:03:57 -0400

    total function

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import Header from '../components/Header'
-import Image from 'next/image'
-import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
-import CheckoutProduct from '../components/CheckoutProduct'
-import Currency from "react-currency-formatter"
+import React from "react";
+import Header from "../components/Header";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItems, selectTotal } from "../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
-  const items = useSelector(selectItems)
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal)
+  const { data: session } = useSession();
   return (
-    <div className='bg-gray-100'>
-      <Header/>
-      <main className='lg:flex max-w-screen-2xl mx-auto'>
-        {/* left */}
-          <div className='flex-grow m-5 shadow-sm'>
-            <Image src="https://links.papareact.com/ikj"
+    <div className="bg-gray-100">
+      <Header />
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
             height={250}
             width={1020}
-            objectFit='contain'/>
-            <div className='flex flex-col p-5 space-y-10 bg-white'>
-              <h1 className='text-3xl border-b pb-4'>{items.length === 0 ? "Your Amazon Basket is empty" : "Shopping Basket"}</h1>
-              {items.map((item, i) => (
-                <CheckoutProduct
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Amazon Basket is empty"
+                : "Shopping Basket"}
+            </h1>
+            {items.map((item, i) => (
+              <CheckoutProduct
                 key={i}
                 id={item.id}
                 title={item.title}
@@ -31,19 +39,35 @@ function Checkout() {
                 category={item.category}
                 image={item.image}
                 hasPrime={item.hasPrime}
-                />
-              ))}
-            </div>
+              />
+            ))}
           </div>
+        </div>
 
-        {/* right */}
-        <div>
-
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):{" "}
+                <span className="font-bold">
+                  <Currency quantity={total} currency="USD"></Currency>
+                </span>
+              </h2>
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  `from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed`
+                }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
         </div>
       </main>
-
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
